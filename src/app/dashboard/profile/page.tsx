7a0becd587a0b0f786abe17654be8c6c93f35607
Mateus Schverz <matefs8569@gmail.com>
2025-03-23 03:50:29 -0300

    fix: traz informações do usuario

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,15 +1,24 @@
 import { getServerSession } from "next-auth";
+import prismaClient from "@/lib/prisma";
 
 const ProfilePage = async () => {
   const session = await getServerSession();
 
   if (session) {
-    return (
-      <div>
-        <h1>Profile Page</h1>
-        <p>Hello {session.user.name}</p>
-      </div>
-    );
+    const user = await prismaClient.user.findUnique({
+      where: { email: session.user.email },
+    });
+
+    if (user) {
+      return (
+        <div>
+          <h1>Profile Page</h1>
+          <p>Hello {user.name}</p>
+          <p>Email: {user.email}</p>
+          <p>Account created at: {user.createdAt.toString()}</p>
+        </div>
+      );
+    }
   }
 };
 
