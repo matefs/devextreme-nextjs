8d06f6c94e379b5bd5f61431545b95408b777afa
Mateus Schverz <matefs8569@gmail.com>
2025-03-23 04:25:47 -0300

    fix: tabela responsiva

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,25 +1,46 @@
 import { getServerSession } from "next-auth";
 import prismaClient from "@/lib/prisma";
+import DataGrid, { Column } from "devextreme-react/data-grid";
 
 const ProfilePage = async () => {
-  const session = await getServerSession();
+  const currentSession = await getServerSession();
 
-  if (session) {
-    const user = await prismaClient.user.findUnique({
-      where: { email: session.user.email },
-    });
+  if (!currentSession) return null;
 
-    if (user) {
-      return (
-        <div>
-          <h1>Profile Page</h1>
-          <p>Hello {user.name}</p>
-          <p>Email: {user.email}</p>
-          <p>Account created at: {user.createdAt.toString()}</p>
-        </div>
-      );
-    }
-  }
+  const userFromDatabase = await prismaClient.user.findUnique({
+    where: { email: currentSession.user.email },
+  });
+
+  const exampleData = [
+    {
+      id: userFromDatabase.id, // Usar o ID do usuário
+      name: userFromDatabase.name,
+      email: userFromDatabase.email,
+      createdAt: userFromDatabase.createdAt.toISOString(), // Formatar a data para string
+    },
+  ];
+
+  if (!userFromDatabase) return null;
+
+  return (
+    <div className="flex justify-center items-center p-4">
+      <DataGrid
+      dataSource={exampleData}
+      showBorders={true}
+      columnAutoWidth={true}
+      className="w-full max-w-full sm:max-w-5xl" // Responsivo: largura máxima ajustada para telas menores
+      allowColumnResizing={true} // Permite redimensionar colunas
+      columnHidingEnabled={true}
+
+      allowColumnReordering={true} // Permite reordenar colunas
+      >
+      <Column dataField="id" caption="ID" minWidth={100} width={150} /> {/* Ajusta largura mínima */}
+      <Column dataField="name" caption="Name" minWidth={150} />
+      <Column dataField="email" caption="Email" minWidth={200} />
+      <Column dataField="createdAt" caption="Created At" minWidth={150} /> {/* Adiciona largura mínima */}
+      </DataGrid>
+    </div>
+  );
 };
 
 export default ProfilePage;
